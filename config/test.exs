ecb38185a25fde00b7f285f052f7618c119fd9d6
Nikko Miu <nikkoamiu@gmail.com>
2016-12-17 06:29:02 -0600

    Fixed Test DB config

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,5 @@ config :logger, level: :warn
 # Configure your database
 config :phoenix_base, PhoenixBase.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "phoenix_base_test",
-  hostname: "localhost",
+  url: "ecto://postgres:postgres@postgres/phoenix_base_test"
   pool: Ecto.Adapters.SQL.Sandbox
