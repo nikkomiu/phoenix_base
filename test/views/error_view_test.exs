084e054787dde024dcc2d620b041cbb526f095cf
Nikko Miu <nikko.miu@nikkomiu.com>
2016-12-28 08:18:41 -0600

    Fixed tests for error views

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -5,17 +5,17 @@ defmodule PhoenixBase.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(PhoenixBase.ErrorView, "404.html", []) ==
-           "Page not found"
+    assert render_to_string(PhoenixBase.ErrorView, "404.html", []) =~
+           "(404)"
   end
 
   test "render 500.html" do
-    assert render_to_string(PhoenixBase.ErrorView, "500.html", []) ==
-           "Internal server error"
+    assert render_to_string(PhoenixBase.ErrorView, "500.html", []) =~
+           "(500)"
   end
 
   test "render any other" do
-    assert render_to_string(PhoenixBase.ErrorView, "505.html", []) ==
-           "Internal server error"
+    assert render_to_string(PhoenixBase.ErrorView, "505.html", []) =~
+           "(500)"
   end
 end
